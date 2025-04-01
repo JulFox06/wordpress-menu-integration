f073160be0d28b1f06577231e28c36194480618d
JulFox06 <renard.julien55@gmail.com>
2025-04-01 22:02:01 +0200

    fix: remove useless script

--- assets/js/components/ExampleComponent.js
+++
@@ -1,8 +0,0 @@
-export default class ExampleComponent {
-  init() {
-    // I am a default component with an accessible init() method/
-
-    // eslint-disable-next-line no-console
-    console.warn('ExampleComponent.js initialized. Please, remove it.');
-  }
-}
